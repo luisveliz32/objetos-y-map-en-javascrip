0530d05e6082667c13e5baeef0e25c8f49cf52e0
luisveliz32@gmail.com <luisveliz32@@gmail.com>
2023-05-11 09:09:18 -0400

    Primer commit

---
+++ objeto.js
@@ -0,0 +1,40 @@
+const persona =[    {
+  
+                        nombre: ['Bob', 'Smith'],
+                        edad: 32,
+                        genero: 'masculino',
+                        intereses: ['música', 'esquí'],
+                    },
+                    {
+                        
+                        nombre: ['Maria', 'Carmen'],
+                        edad: 31,
+                        genero: 'Femenino',
+                        intereses: ['música', 'Bailar'],
+                    },
+                    {
+  
+                        nombre: ['Bob', 'Smith'],
+                        edad: 32,
+                        genero: 'masculino',
+                        intereses: ['música', 'esquí'],
+                    },
+                    {
+  
+                        nombre: ['Bob', 'Smith'],
+                        edad: 32,
+                        genero: 'masculino',
+                        intereses: ['música', 'esquí'],
+                    },
+                    {
+  
+                        nombre: ['Bob', 'Smith'],
+                        edad: 32,
+                        genero: 'masculino',
+                        intereses: ['música', 'esquí'],
+                    },
+                    
+                ] 
+persona.map(function(per){
+    console.log('hola soy'+per.nombre);
+});
